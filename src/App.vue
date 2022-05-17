697a2e4b7af71e0ec184c39aea132cb3b0b3f59c
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-05-17 09:52:22 +0200

    bonne-base

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 
 <template>
   <section class="bg-indigo-50">
-    <h1 class="text-center text-xl ">TP Agence Immobilière</h1>
-      <header>
-        <div class="p-3 mt-5 mb-5 bg-gray-50 flex flex-row justify-between">
-          <div >
-            <MailOpenIcon class="inline-block w-10"/>
-            <p class="inline-block ml-4 mr-4 text-lg text-blue-600  ">Estatery</p>
-          </div>
-          <MenuAlt4Icon class="inline-block text-right w-8"/>
+    <header>
+      <div class="p-3 mb-5 bg-gray-50 flex flex-row justify-between">
+        <div >
+          <MailOpenIcon class="inline-block w-10"/>
+          <h1 class="inline-block ml-4 mr-4 text-lg text-blue-600  ">Estatery</h1>
         </div>
-      </header>
-      <main>
-        <h2 class="text-lg font-bold text-center mb-4">Search proprieties to rent</h2>
-        <section class="overflow-hidden flex items-center justify-center ">
-          <div class="pt-2 relative mx-auto text-gray-600">
-            <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-              type="search" name="search" placeholder="Search location">
-            <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
-              <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
-                viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
-                width="512px" height="512px">
-                <path
-                  d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-              </svg>
-            </button>
-          </div>
-        </section>
-        <section>
-        </section>
-        
+        <MenuAlt4Icon class="inline-block text-right w-8"/>
+      </div>
+    </header>
+    <main>
+      <h2 class="text-lg font-bold text-center mb-4">Search proprieties to rent</h2>
+      <section class="overflow-hidden flex items-center justify-center ">
+        <div class="pt-2 relative mx-auto text-gray-600">
+          <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search" name="search" placeholder="Search location">
+          <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+            <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+              viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+              width="512px" height="512px">
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
+        </div>
+      </section>
+      <section>
+      </section>
+      
         <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
             <card 
               :nbrbath="1"
